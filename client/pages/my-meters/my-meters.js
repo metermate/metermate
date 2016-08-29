57aeff08b5ddd6f54c7ab0473505402f778771ba
Christian Borja <cpborja32@gmail.com>
2016-08-29 14:10:27 -0700

    Revert "sets up client-side file structure and routes; creates initial google map with dummy markers showing"

--- client/pages/my-meters/my-meters.js
+++
@@ -1,7 +0,0 @@
-angular
-  .module('metermate.my-meters', [])
-  .controller('myMetersCtrl', function($scope) {
-    $scope.testing = 'My Meter is connected' //Change this; This is to check that the route is working;
-  });
-
-//Bonus feature 😬
