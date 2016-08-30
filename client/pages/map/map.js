4ca5f83754d5be912bd13f4a2e9b866d62253226
Sam Kim <samkim28@gmail.com>
2016-08-29 20:52:29 -0700

    [feature] adds additional dummy data for info window feature

--- client/pages/map/map.js
+++ client/pages/map/map.js
@@ -7,25 +7,50 @@ angular
     var locations = [
       {
       meterlat: 34.01633,
-      meterlong: -118.49191
+      meterlong: -118.49191,
+      name: 'broadway1',
+      active: 'active',
+      area: '3rd street promenade area',
+      street_address: '12 broadway'
       },
       {
       meterlat: 34.01103,
-      meterlong: -118.49176
+      meterlong: -118.49176,
+      name: 'broadway2',
+      active: 'active',
+      area: 'near ROC',
+      street_address: '123 broadway'
+
       },
       {
       meterlat: 34.03303,
-      meterlong: -118.48035
+      meterlong: -118.48035,
+      name: 'broadway3',
+      active: 'inactive',
+      area: 'downtown',
+      street_address: '1234 broadway'
+
       },
       {
       meterlat: 34.03224,
-      meterlong: -118.47578
+      meterlong: -118.47578,
+      name: 'broadway4',
+      active: 'active',
+      area: 'the pier',
+      street_address: '12345 broadway'
+
       },
       {
       meterlat: 34.02135,
-      meterlong: -118.49573
+      meterlong: -118.49573,
+      name: 'broadway5',
+      active: 'inactive',
+      area: 'next to mendocino',
+      street_address: '123456 broadway'
+
       }
     ] //This is our dummy data, btw.
+
 
     var mapProp = {
       center: $scope.defaultLocation,
