7a1be5874132abc54099d349241bf8bd6ce700d1
Sam Chi <samuel.sj.chi@gmail.com>
2016-08-30 01:01:49 -0700

    [setup] adds third api endpoint to retrieve meter data from mysql database

--- server/server.js
+++ server/server.js
@@ -16,31 +16,29 @@ app.use(express.static('./client'));
 app.set('port', process.env.PORT || 1337);
 
 dbConnection.connect(function(err) {
-  if (!err) {
-    console.log('Database is connected...');
-    dbConnection.query('DROP TABLE IF EXISTS meters')
-    dbConnection.query('CREATE TABLE meters (meter_id varchar(100), latitude varchar(100), longitude varchar(100), active varchar(100), area varchar(100), street_address varchar(100), event_type varchar(25), event_time varchar(100))', function(err, result){
-      if (err) {
-        console.log('Error creating the table: Meters', err);
-      } else {
-        console.log('Table: Meters created successfully!');
-      }
-    })
+  if (err) {
+    console.error('Error connecting to meterDB: ', err);
   } else {
-    console.log('Error connecting to database...');
+    console.log('Successfully connected to meterDB');
+    dbConnection.query('CREATE TABLE IF NOT EXISTS meters (meter_id varchar(100), latitude varchar(100), longitude varchar(100), active varchar(100), area varchar(100), street_address varchar(100), event_type varchar(25), event_time varchar(100))', function(err, result){
+      if (err) {
+        console.log('Error creating table in meterDB: ', err);
+      } else {
+        console.log('Successfully created table in meterDB');
+      }
+    });
   }
 });
 
 app.get('/api/meter-locations', function(req, res) {
   var url = 'https://parking.api.smgov.net/meters/';
-
   request.get(url, function(error, response, body) {
-    dbConnection.query('TRUNCATE meters');
     if (error) {
       res.status(400).send(error);
     }
     body = JSON.parse(body);
 
+    dbConnection.query('TRUNCATE meters');
     for (var i = 0; i < body.length; i++) {
       dbConnection.query('INSERT INTO meters (meter_id, latitude, longitude, active, area, street_address) VALUES (?,?,?,?,?,?)', [body[i].meter_id, body[i].latitude, body[i].longitude, body[i].active, body[i].area, body[i].street_address], function(err, result) {
         if (err) {
@@ -61,12 +59,12 @@ app.get('/api/meter-events', function(req, res) {
   var finalDate = parsedDate.substr(1, 15).concat('Z');
 
   var url = 'https://parking.api.smgov.net/meters/events/since/' + finalDate;
-
   request.get(url, function(error, response, body) {
-    body = JSON.parse(body);
     if (error) {
       res.status(400).send(error);
     }
+    body = JSON.parse(body);
+
     for (var i = 0; i < body.length; i++) {
       dbConnection.query('UPDATE meters SET event_type = ?, event_time = ? WHERE meter_id = ?', [body[i].event_type, body[i].event_time, body[i].meter_id], function(err, result) {
         if (err) {
@@ -76,6 +74,16 @@ app.get('/api/meter-events', function(req, res) {
     }
     res.status(200).json(body);
   })
+});
+
+app.get('/api/get-meter-data', function(req, res) {
+  dbConnection.query('SELECT * FROM meters WHERE event_type IN ("SS","SE") AND latitude != 0', function(err, data) {
+    if (err) {
+      console.error('Error retrieving data from meterDB: ', err);
+    } else {
+      res.json(data);
+    }
+  });
 });
 
 app.listen(app.get('port'), function() {
