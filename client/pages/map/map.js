1c7df4bfeada05e29ade456e921a8fd6930899e2
Sam Chi <samuel.sj.chi@gmail.com>
2016-08-31 23:37:55 -0700

    [fix] fixes bug causing markers to move when info window opens

--- client/pages/map/map.js
+++ client/pages/map/map.js
@@ -18,20 +18,18 @@ angular
 
           /* ---------- MARKERS ---------- */
           var markers = [];
+          var meterIcon = {
+            path: 'M-276.6,355.8c-8,0-14.4,6.5-14.4,14.4c0,4.8,2.4,9.1,6,11.7c1.3,10.2,3.9,13.7,3.9,13.7c0,0.4,0.3,0.4,0.6,0.4h2.1v39.3c-1.7,0.1-3.3,0.4-3.3,0.8c0,0.5,2.3,0.9,4.9,0.9c2.6,0,4.9-0.4,4.9-0.9c-0.1-0.4-1.5-0.7-3.5-0.8V396h2.4c0.3,0,0.6-0.1,0.6-0.4c0,0,2.6-3.4,3.9-13.6c3.7-2.6,6.2-6.9,6.2-11.8C-262.3,362.3-268.7,355.9-276.6,355.8zM-275.4,378.8c0,0.8-0.7,1.4-1.5,1.4s-1.5-0.6-1.5-1.4v-5.6c0-0.8,0.7-1.4,1.5-1.4s1.5,0.6,1.5,1.4V378.8zM-268,367.6c-1.2-3.7-4.7-6.6-8.7-6.6c-4.2,0-7.7,2.8-8.7,6.6c-1.1-0.3-2-0.6-2.7-0.9c1.4-5,6-8.7,11.5-8.7c5.4,0,10,3.6,11.4,8.6C-265.9,366.9-266.8,367.2-268,367.6z',
+            fillOpacity: 0.8,
+            scale: 0.65,
+            strokeColor: '#425765',
+            strokeWeight: 1
+          };
 
           for (var i = 0; i < meterData.length; i++) {
-            var defaultMeterIcon = {
-              path: 'M-276.6,355.8c-8,0-14.4,6.5-14.4,14.4c0,4.8,2.4,9.1,6,11.7c1.3,10.2,3.9,13.7,3.9,13.7c0,0.4,0.3,0.4,0.6,0.4h2.1v39.3c-1.7,0.1-3.3,0.4-3.3,0.8c0,0.5,2.3,0.9,4.9,0.9c2.6,0,4.9-0.4,4.9-0.9c-0.1-0.4-1.5-0.7-3.5-0.8V396h2.4c0.3,0,0.6-0.1,0.6-0.4c0,0,2.6-3.4,3.9-13.6c3.7-2.6,6.2-6.9,6.2-11.8C-262.3,362.3-268.7,355.9-276.6,355.8zM-275.4,378.8c0,0.8-0.7,1.4-1.5,1.4s-1.5-0.6-1.5-1.4v-5.6c0-0.8,0.7-1.4,1.5-1.4s1.5,0.6,1.5,1.4V378.8zM-268,367.6c-1.2-3.7-4.7-6.6-8.7-6.6c-4.2,0-7.7,2.8-8.7,6.6c-1.1-0.3-2-0.6-2.7-0.9c1.4-5,6-8.7,11.5-8.7c5.4,0,10,3.6,11.4,8.6C-265.9,366.9-266.8,367.2-268,367.6z',
-              fillColor: '#FF6666',
-              fillOpacity: 0.8,
-              scale: 0.65,
-              strokeColor: '#425765',
-              strokeWeight: 1
-            };
             // creates marker for each meter in meterData array
             var marker = new google.maps.Marker({
               position: new google.maps.LatLng(meterData[i].latitude, meterData[i].longitude),
-              icon: defaultMeterIcon,
               id: meterData[i].meter_id,
               active: meterData[i].active,
               area: meterData[i].area,
@@ -52,14 +50,15 @@ angular
             var addressStr = marker.street_address;
             marker.street_address = addressStr.split(' ').map(word => word[0].toUpperCase() + word.substring(1).toLowerCase()).join(' ');
 
-            // shows green meter icon if meter is available
+            // shows green meter if available, red meter if occupied
             if(meterData[i].event_type === 'SE') {
               console.log('<-- # of available meters');
-              var greenMeterIcon = defaultMeterIcon;
-              greenMeterIcon.fillColor = '#10D492';
-              marker.setIcon(greenMeterIcon);
+              meterIcon.fillColor = '#10D492';
+              marker.setIcon(meterIcon);
               marker.status = 'Available';
             } else {
+              meterIcon.fillColor = '#FF6666';
+              marker.setIcon(meterIcon);
               marker.status = 'Occupied';
             }
 
@@ -92,7 +91,10 @@ angular
                     addressControl: false,
                     linksControl: false
                   });
-                  pano.bindTo('position', marker);
+
+                  var pin = new google.maps.MVCObject();
+                  pin.set('position', marker.getPosition());
+                  pano.bindTo('position', pin);
                   pano.setVisible(true);
                 });
 
