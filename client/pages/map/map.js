f21d20514045580c6a7d1a2e8577d47bd0121a7f
Sam Chi <samuel.sj.chi@gmail.com>
2016-08-31 20:12:21 -0700

    [feature] replaces default marker with custom meter icon and shows meter availability

--- client/pages/map/map.js
+++ client/pages/map/map.js
@@ -20,9 +20,18 @@ angular
           var markers = [];
 
           for (var i = 0; i < meterData.length; i++) {
+            var redMeter = {
+              path: 'M-276.6,355.8c-8,0-14.4,6.5-14.4,14.4c0,4.8,2.4,9.1,6,11.7c1.3,10.2,3.9,13.7,3.9,13.7c0,0.4,0.3,0.4,0.6,0.4h2.1v39.3c-1.7,0.1-3.3,0.4-3.3,0.8c0,0.5,2.3,0.9,4.9,0.9c2.6,0,4.9-0.4,4.9-0.9c-0.1-0.4-1.5-0.7-3.5-0.8V396h2.4c0.3,0,0.6-0.1,0.6-0.4c0,0,2.6-3.4,3.9-13.6c3.7-2.6,6.2-6.9,6.2-11.8C-262.3,362.3-268.7,355.9-276.6,355.8zM-275.4,378.8c0,0.8-0.7,1.4-1.5,1.4s-1.5-0.6-1.5-1.4v-5.6c0-0.8,0.7-1.4,1.5-1.4s1.5,0.6,1.5,1.4V378.8z M-268,367.6c-1.2-3.7-4.7-6.6-8.7-6.6c-4.2,0-7.7,2.8-8.7,6.6c-1.1-0.3-2-0.6-2.7-0.9c1.4-5,6-8.7,11.5-8.7c5.4,0,10,3.6,11.4,8.6C-265.9,366.9-266.8,367.2-268,367.6z',
+              fillColor: '#FF6666',
+              fillOpacity: 0.8,
+              scale: 0.7,
+              strokeColor: '#425765',
+              strokeWeight: 1
+            };
             // creates marker for each meter in meterData array
             var marker = new google.maps.Marker({
               position: new google.maps.LatLng(meterData[i].latitude, meterData[i].longitude),
+              icon: redMeter,
               id: meterData[i].meter_id,
               active: meterData[i].active,
               area: meterData[i].area,
@@ -44,10 +53,18 @@ angular
             marker.street_address = addressStr.split(' ').map(word => word[0].toUpperCase() + word.substring(1).toLowerCase()).join(' ');
 
             // sets green marker if meter is available
+            var greenMeterIcon = {
+              path: 'M-276.6,355.8c-8,0-14.4,6.5-14.4,14.4c0,4.8,2.4,9.1,6,11.7c1.3,10.2,3.9,13.7,3.9,13.7c0,0.4,0.3,0.4,0.6,0.4h2.1v39.3c-1.7,0.1-3.3,0.4-3.3,0.8c0,0.5,2.3,0.9,4.9,0.9c2.6,0,4.9-0.4,4.9-0.9c-0.1-0.4-1.5-0.7-3.5-0.8V396h2.4c0.3,0,0.6-0.1,0.6-0.4c0,0,2.6-3.4,3.9-13.6c3.7-2.6,6.2-6.9,6.2-11.8C-262.3,362.3-268.7,355.9-276.6,355.8zM-275.4,378.8c0,0.8-0.7,1.4-1.5,1.4s-1.5-0.6-1.5-1.4v-5.6c0-0.8,0.7-1.4,1.5-1.4s1.5,0.6,1.5,1.4V378.8z M-268,367.6c-1.2-3.7-4.7-6.6-8.7-6.6c-4.2,0-7.7,2.8-8.7,6.6c-1.1-0.3-2-0.6-2.7-0.9c1.4-5,6-8.7,11.5-8.7c5.4,0,10,3.6,11.4,8.6C-265.9,366.9-266.8,367.2-268,367.6z',
+              fillColor: '#10D492',
+              fillOpacity: 0.8,
+              scale: 0.7,
+              strokeColor: '#425765',
+              strokeWeight: 1
+            };
             if(meterData[i].event_type === 'SE') {
               console.log('<-- # of available meters');
               marker.status = 'Available';
-              marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+              marker.setIcon(greenMeterIcon);
             } else {
               marker.status = 'Occupied';
             }
