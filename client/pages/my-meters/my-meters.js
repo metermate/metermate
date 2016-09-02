49b06b0714afb1dbb705beea633cf20a9abf6c85
justsandytran <just.sandy.tran@gmail.com>
2016-09-02 14:29:18 -0700

    [delete] delete the my-meter and profile pages

--- client/pages/my-meters/my-meters.js
+++
@@ -1,5 +0,0 @@
-angular
-  .module('metermate.my-meters', [])
-  .controller('MyMetersCtrl', function($scope) {
-    $scope.testing = 'My Meter is connected' //Change this; This is to check that the route is working;
-  });
--- client/pages/profile/profile.js
+++
@@ -1,5 +0,0 @@
-angular
-  .module('metermate.profile', [])
-  .controller('ProfileCtrl', function($scope) {
-    $scope.testing = 'Profile is connected' //Change this; This is to check that the route is working;
-  });
