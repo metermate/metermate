8f04440c3ba8b7513a06a9d3210eed781bb350c3
justsandytran <just.sandy.tran@gmail.com>
2016-09-02 18:41:01 -0700

    [setup] Added an about container for each engineer

--- client/pages/about/about.js
+++ client/pages/about/about.js
@@ -5,30 +5,31 @@ angular
       {
         name: "Sandy Tran",
         img:"",
-        description: "Likes dogs",
+        description: "Web Developer",
         github:"https://github.com/justsandytran",
         linkedIn:"https://www.linkedin.com/in/sandy-tran-253a78b5?trk=hp-identity-photo"
+
       },
       {
         name: "Sam Kim",
         img:"",
-        description: "Likes coffee",
-        github:"https://github.com/samkim28",
-        linkedIn:""
+        description: "Web Developer",
+        github: "https://github.com/samkim28",
+        linkedIn: "https://www.linkedin.com/in/samkim28"
       },
       {
         name: "Christian Borja",
         img:"",
-        description: "Likes beer",
+        description: "Scrum Master",
         github:"https://github.com/cborjah",
-        linkedIn:""
+        linkedIn:"www.linkedin.com/in/christianborja"
       },
       {
         name: "Sam Chi",
         img:"",
-        description: "Likes photography",
+        description: "Product Owner",
         github:"https://github.com/samsjchi",
-        linkedIn:""
+        linkedIn:"https://www.linkedin.com/in/samuelsjchi"
       }
     ]
 
