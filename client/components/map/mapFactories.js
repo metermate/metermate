0b1dcfcad6c7d5e493c94abcf3d177f19764750d
Sam Chi <samuel.sj.chi@gmail.com>
2016-09-05 17:02:15 -0700

    [refactor] combines map and geolocation factories into one file

--- client/components/map/mapFactories.js
+++ client/components/map/mapFactories.js
@@ -1,5 +1,29 @@
 angular
   .module('metermate.map')
+  .factory('Map', function($http) {
+    return {
+      getMeterData: getMeterData
+    };
+
+    function getMeterData(param) {
+      return $http({
+        method: 'GET',
+        url: '/api/meters/latest-data',
+        params: {
+          swLat: param.swLat,
+          swLng: param.swLng,
+          neLat: param.neLat,
+          neLng: param.neLng
+        }
+      })
+        .then(function(response) {
+          return response.data;
+        })
+        .catch(function(error) {
+          console.error(error);
+        });
+    };
+  });
   .factory('Geolocation', function(){
     return {
       centerMap: centerMap
