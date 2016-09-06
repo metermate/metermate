536b57222d968b74cfc52bbcc86126ff20313a54
justsandytran <just.sandy.tran@gmail.com>
2016-09-05 23:40:01 -0700

    [refactor] refactor routes

--- client/app/app.js
+++ client/app/app.js
@@ -1,17 +1,19 @@
 angular
   .module('metermate', ['ui.router', 'metermate.home', 'metermate.map'])
-  .config(function($urlRouterProvider, $stateProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: '/app/home/homeView.html',
-        controller: 'HomeCtrl'
-      })
-      .state('map', {
-        url: '/map',
-        templateUrl: '/app/map/mapView.html',
-        controller: 'MapCtrl'
-      });
+  .config(config);
 
-    $urlRouterProvider.otherwise('/');
-  });
+function config($urlRouterProvider, $stateProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: '/app/home/homeView.html',
+      controller: 'HomeCtrl'
+    })
+    .state('map', {
+      url: '/map',
+      templateUrl: '/app/map/mapView.html',
+      controller: 'MapCtrl'
+    });
+
+  $urlRouterProvider.otherwise('/');
+}
