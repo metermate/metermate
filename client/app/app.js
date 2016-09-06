8d44ff98972b65767add697eda5a90e96b64dfe2
justsandytran <just.sandy.tran@gmail.com>
2016-09-06 11:35:12 -0700

    [fix] made changes

--- client/app/app.js
+++ client/app/app.js
@@ -1,19 +1,18 @@
 angular
   .module('metermate', ['ui.router', 'metermate.home', 'metermate.map'])
-  .config(config);
+    .config(function ($urlRouterProvider, $stateProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: '/app/home/homeView.html',
+          controller: 'HomeCtrl'
+        })
+        .state('map', {
+          url: '/map',
+          templateUrl: '/app/map/mapView.html',
+          controller: 'MapCtrl'
+        });
 
-function config($urlRouterProvider, $stateProvider) {
-  $stateProvider
-    .state('home', {
-      url: '/',
-      templateUrl: '/app/home/homeView.html',
-      controller: 'HomeCtrl'
-    })
-    .state('map', {
-      url: '/map',
-      templateUrl: '/app/map/mapView.html',
-      controller: 'MapCtrl'
-    });
-
-  $urlRouterProvider.otherwise('/');
-}
+      $urlRouterProvider.otherwise('/');
+    }
+);
